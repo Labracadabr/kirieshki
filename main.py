0fdcd08d0f480a80a4a54435a203dbac8abba790
Dmitrii Minokin <dminokin@gmail.com>
2023-03-28 16:08:28 +0300

    bot

--- main.py
+++ main.py
@@ -1,16 +1,140 @@
-# This is a sample Python script.
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command, Text
+from aiogram.types import Message
+from datetime import datetime
+import random
+from random import choice
+import json
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+BOT_TOKEN: str = '1541721067:AAH7YRcfoG_YIDH8li2NTrI7TtR_y5TS-FM'
+# Создаем объекты бота и диспетчера
+bot: Bot = Bot(BOT_TOKEN)
+dp: Dispatcher = Dispatcher()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def rule(w):
+    w = str(w)
+    for k in range(len(w)):
+        if w[-k - 1] in first_letters:
+            return w[-k - 1]
 
 
-# Press the green button in the gutter to run the script.
+with open("cities_rus.json", "r") as f:
+    cities = json.load(f)
+first_letters = [
+    'а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц',
+    'ч', 'ш', 'ы', 'э', 'ю', 'я']
+
+dlina = 0
+for i in cities:
+    dlina += len(cities.get(i))
+used = []
+global bot_word
+
+
+# /start
+@dp.message(Command(commands=['start']))
+async def process_start_command(message: Message):
+    with open("add.txt", "a", encoding='utf-8') as w:
+        w.write(f'\n\n{datetime.now().strftime("%d/%m/%Y %H:%M:%S")},'
+                f' id {message.from_user.id}, @{message.from_user.username}, '
+                f'{message.from_user.first_name} {message.from_user.last_name}\n')
+
+    global bot_word
+    used.clear()
+    await message.answer('Привет!\nДавай сыграем в города.\n'
+                         f'Я знаю городов: {dlina}.\n\nМожешь отправить:\n/start для рестарта\n'
+                         f'/add для добавления городов\n/help для чтения правил'
+                         f'\n/stop для завершения игры и просмотра итога.')
+    r = choice(first_letters)
+    bot_word = choice(cities[r])
+    used.append(bot_word)
+    await message.answer(f'Я начну: {bot_word}')
+    with open("add.txt", "a", encoding='utf-8') as w:
+        w.write(f'{bot_word.upper()} ')
+
+
+# конец игры
+@dp.message(Command(commands=['stop']))
+async def process_cancel_command(message: Message):
+    with open("add.txt", "a", encoding='utf-8') as w:
+        w.write('/stop ')
+    if used:
+        end = ''
+        for j in used:
+            end += (j + ', ')
+        await message.answer(f'Конец, мы назвали {len(used)} города(ов): {end[:-2]}.')
+        used.clear()
+    else:
+        await message.answer('Так мы еще не начали, жми /start')
+
+
+# help
+@dp.message(Command(commands=['help']))
+async def process_cancel_command(message: Message):
+    with open("add.txt", "a", encoding='utf-8') as w:
+        w.write('/help ')
+    if used:
+        await message.answer(f'Отправь мне город, начинающийся на последнюю букву моего города. Если это Ь или Ъ - то '
+                             f'бери следующую с конца букву.\n'
+                             f'А на букву Ы, если что, я знаю {len(cities["ы"])} городов.'
+                             f'\n\nСейчас тебе на {rule(bot_word).upper()}!')
+    else:
+        await message.answer(f'Отправь мне город, начинающийся на последнюю букву моего города. Если это Ь или Ъ - то '
+                             f'бери следующую с конца букву.\n'
+                             f'А на букву Ы, если что, я знаю {len(cities["ы"])} городов.')
+
+
+# добаление городов
+@dp.message(Command(commands=['add']))
+async def process_negative_answer(message: Message):
+    with open("add.txt", "a", encoding='utf-8') as w:
+        w.write('/add ')
+    await message.answer('Функция пока не работает :(')
+    # del message.text
+    # await message.answer('Вводи города одним сообщением через пробел, чтобы добавить их в мою базу')
+    # adds = str(message.text).split()
+    # with open("add.txt", "a", encoding='utf-8') as w:
+    #     w.write(*adds)
+    #     w.write('\n\n')
+    # await message.answer(f'Внесено городов на рассмотрение моим хозяином: {len(adds)}.')
+
+
+# если ввод не на доступную букву
+@dp.message(lambda x: x.text and rule(x) not in first_letters)
+async def process_text_answer(message: Message):
+    with open("add.txt", "a", encoding='utf-8') as w:
+        w.write(f'{str(message.text)} ')
+    await message.answer(f'Я могу только по-русски, тебе на {rule(bot_word).upper()}')
+
+
+# остальные любые сообщения
+@dp.message()
+async def process_other_text_answers(message: Message):
+    global bot_word
+    player_word = str(message.text)
+    adds = str(message.text)
+    with open("add.txt", "a", encoding='utf-8') as w:
+        # w.write(f'{str(message.from_user.id), str(message.from_user.first_name), str(message.from_user.last_name)}')
+        w.write(f'{adds.capitalize()} ')
+
+    if player_word[0].lower() == rule(bot_word).lower():
+        if player_word.upper() in used or player_word.capitalize() in used:
+            await message.answer('Этот город уже был!')
+        elif player_word.capitalize() in cities[player_word.lower()[0]]:
+            used.append(player_word.capitalize())
+            while bot_word in used or bot_word[0].lower() != rule(player_word).lower():
+                bot_word = choice(cities[rule(player_word)])
+            await message.answer(bot_word)
+            with open("add.txt", "a", encoding='utf-8') as w:
+                w.write(f'{bot_word.upper()} ')
+            player_word = ''
+            used.append(bot_word)
+        else:
+            await message.answer(f'Не знаю такого, попоробуй еще - тебе на {rule(bot_word).upper()}')
+    else:
+        await message.answer(f'Первая буква не подходит, тебе на {rule(bot_word).upper()}')
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    dp.run_polling(bot)
